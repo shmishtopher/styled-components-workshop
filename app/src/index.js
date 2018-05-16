7b8e16464467acf8732829f1932eccf8edd4c7af
shmishtopher <shmish90@gmail.com>
2018-05-16 18:02:12 -0400

    fix

--- app/src/index.js
+++ app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './components/App'
 
-export const secretKey = '8ea581e415a94797f52d1aea3f53410e'  // Example key, use your own
+export const secretKey = '5d3624cfd3c28db0011d513924c4f6c4'  // Example key, use your own
 export const getCoords = cb => navigator.geolocation.getCurrentPosition(cb)
 export const baseurl = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/'
 
